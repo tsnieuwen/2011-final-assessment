e391a39f3693cd980329485ee8f3ae129e60cd57
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-03-08 10:34:48 -0700

    Application code written for all of story 2

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -2,6 +2,9 @@ class SurgeriesController < ApplicationController
 
   def show
     @surgery = Surgery.find(params[:id])
+    @all_doctors = @surgery.doctors
+    @experienced_docs = @surgery.most_experienced_doctors
+    @inexperienced_docs = @surgery.least_experienced_doctors
   end
 
 end
--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -3,4 +3,12 @@ class Surgery < ApplicationRecord
   has_many :doctor_surgeries
   has_many :doctors, through: :doctor_surgeries
 
+  def most_experienced_doctors
+    doctors.order(years_practiced: :desc).limit(2)
+  end
+
+  def least_experienced_doctors
+    doctors.order(years_practiced: :asc).limit(2)
+  end
+
 end
